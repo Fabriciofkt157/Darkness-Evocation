3a7ee8ec4d7e761accd0c092ce30eff17af1416e
Fabriciofkt157 <99661346+Fabriciofkt157@users.noreply.github.com>
2023-05-01 18:15:34 -0300

    Update main.js

--- libs/main.js
+++ libs/main.js
@@ -14,7 +14,7 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const mtlLoader = new THREE.MTLLoader();
+const mtlLoader = new MTLLoader();
 // Carregue o material e o modelo
 mtlLoader.load('materials.mtl', function(materials) {
   materials.preload();
